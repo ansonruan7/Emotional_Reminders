088747b75f89748c1995dc600d95c004f60b1a01
ansonruan7 <ansonruan4@gmail.com>
2025-02-24 14:11:44 -0500

    minor styling details

--- app/index.tsx
+++ app/index.tsx
@@ -2,15 +2,16 @@ import { View, Text, StyleSheet, ImageBackground, TextInput, Pressable, ScrollVi
 import { SafeAreaView } from 'react-native-safe-area-context'
 import React from 'react'
 import { Link } from 'expo-router'
+import '../global.css'
 
 import CustomButton from '../components/CustomButton'
 
 const app = () => {
   return (
-    <SafeAreaView className='bg-primary'>
+    <SafeAreaView className='bg-primary h-full'>
       <ScrollView className="h-[100%]">
-        <View className='h-full px-4 items-center justify-center'>
-          <Text className='text-white text-6xl'>Hello</Text>
+        <View className='h-full px-4 items-center justify-center border-2 border-solid'>
+          <Text className='text-white text-6xl font-extrabold my-12'>insert_name</Text>
           <CustomButton
             title = "Continue With Email"
             handlePress = {() => {}}
@@ -18,6 +19,7 @@ const app = () => {
             textStyles={{}}
             isLoading={false}
           />
+          <Link href='/(tabs)/home' className='my-2'>Already have an account?</Link>
         </View>
       </ScrollView>
     </SafeAreaView>
